90b5de48773125fccd5a8ad3e90becda323e98aa
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2024-09-18 15:31:37 +0800

    Upstream to latest (#1)
    * chore: bump aliuhook to support A14 QPR2
    
    * automatically set thread related experiments
    
    * enable text in voice experiment
    
    * fix voice and fix NPE in default avatars patch
    
    * fix persisting channels and messages
    
    * fix displaying display names
    
    * Revert "fix displaying display names"
    
    This reverts commit 11ba2240b79cf71d24c6ba72c492ddb3097885e8.
    
    Apparently it causes issues on older devices, because required patches for fixing parsing aren't working.
    
    * chore: bump aliuhook to support A14+
    
    * installer: bump all dependencies
    
    * installer: add ManagerDialog that will be displayed when Manager is released
    
    * installer: enable proguard
    
    * add required java 17 setup for installer workflow
    
    * feat(CI): rewrite build workflows
    
    - Fix possible race conditions
    - Better token security
    
    * feat: smali patch tooling
    
    Tooling for writing and distributing versioned smali patch packages
    
    * feat: make core/injector versioned
    
    Old core used commit hash in order to update, which forced a core update when there weren't any changes to core other than the commit hash.
    
    * fix(patches): make reproducible builds
    
    Originally the zip had timestamps which caused a new patch package to be committed every single build.
    
    * feat: denote custom builds in debug info
    
    * feat: installation components versions API
    
    Manager injects a config file into the APK with the versions of builds that were used to patch & install the APK. This parses and exposes that data.
    
    * fix(CI): only deploy builds if version changes
    
    * feat: integrate coreplugins into PluginManager
    
    * refactor: small clean up
    
    * feat: handle coreplugins on plugins screen
    
    * feat: allow plugin authors to disable linking profile
    
    * fix: coreplugins no longer start twice
    
    * feat(NoTrack): allow disabling plugin
    
    * fix: render markdown in plugin descriptions
    
    * Revert "feat(NoTrack): allow disabling plugin"
    
    This reverts commit f8e8424d30c4ac16a460b4513dee554199b99b96.
    
    * ask for all files permission when needed
    
    * refactor(CorePlugins): avoid companion objects
    
    This is dumb but whatever
    
    * feat: add toggle for showing built-in plugins (#477)
    
    * feat(SupporterBadges): rewrite & clean up (#476)
    
    * feat: set experiment defaults (#478)
    
    Add more experiment defaults after Discord stopped deleted them for v126.21
    
    * feat(CorePlugins): clean up/minor fixes
    
    - Reduce usage of old Patcher api
    - Stop patching on load() when unnecessary
    - Other minor improvements
    
    * add (Custom) to version in settings
    
    * patch possible crash on Android 15 (?) and bump to 2.0.1
    
    reported on Android 15 Beta 4
    
    * feat(patches): deployWithAdb
    
    ---------
    
    Co-authored-by: rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
    Co-authored-by: Juby210 <31005896+Juby210@users.noreply.github.com>

--- Aliucord/src/main/java/com/aliucord/coreplugins/rn/RNAPI.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/rn/RNAPI.kt
@@ -1,19 +1,26 @@
 /*
  * This file is part of Aliucord, an Android Discord client mod.
- * Copyright (c) 2023 Juby210 & Vendicated
+ * Copyright (c) 2024 Juby210 & Vendicated
  * Licensed under the Open Software License version 3.0
  */
 
 package com.aliucord.coreplugins.rn
 
 import android.content.Context
-import com.aliucord.entities.Plugin
+import com.aliucord.entities.CorePlugin
+import com.discord.api.channel.Channel
 import com.discord.api.user.User
 import com.discord.api.user.UserProfile
+import com.discord.models.message.Message
 import de.robv.android.xposed.XposedBridge
 
-class RNAPI : Plugin(Manifest("RNAPI")) {
+internal class RNAPI : CorePlugin(Manifest("RNAPI")) {
+    override val isHidden = true
+    override val isRequired = true
+
     override fun load(context: Context?) {
+        XposedBridge.makeClassInheritable(Channel::class.java)
+        XposedBridge.makeClassInheritable(Message::class.java)
         XposedBridge.makeClassInheritable(User::class.java)
         XposedBridge.makeClassInheritable(UserProfile::class.java)
 
@@ -23,6 +30,8 @@ class RNAPI : Plugin(Manifest("RNAPI")) {
         patchDefaultAvatars()
         patchUsername()
         patchStickers()
+        patchVoice()
+        fixPersisters()
     }
 
     override fun start(context: Context?) {}
